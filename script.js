9c8ab30b30d926e96ec7b4914a5023baf991ade0
Nishad Mansoor <nishadm9802@gmail.com>
2026-07-29 00:17:24 -0400

    updated element location

--- script.js
+++ script.js
@@ -18,6 +18,7 @@ const line2 = document.getElementById('line2');
 const line3 = document.getElementById('line3');
 const explore = document.getElementById('explore');
 
+//hero page
 typeText(line1, "Hello! Welcome to my portfolio", 60, function () {
     setTimeout(function () {
         typeText(line2, "My name is Nishad", 60, function () {
