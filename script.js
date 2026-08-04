72d135584290a38d7707e0e9bbaa28ebef410547
Nishad Mansoor <nishadm9802@gmail.com>
2026-08-04 19:01:44 -0400

    update intro text

--- script.js
+++ script.js
@@ -19,11 +19,11 @@ const line3 = document.getElementById('line3');
 const explore = document.getElementById('explore');
 
 //hero page
-typeText(line1, "Hello! Welcome to my portfolio", 60, function () {
+typeText(line1, "hey there! my name is nishad", 60, function () {
     setTimeout(function () {
-        typeText(line2, "My name is Nishad", 60, function () {
+        typeText(line2, "welcome to my portfolio", 60, function () {
             setTimeout(function () {
-                typeText(line3, "See what I've been building", 60, function () {
+                typeText(line3, "click here to learn more about me & my work!", 60, function () {
                     explore.style.display = 'block';
                 }, 300);
             }, 400);
