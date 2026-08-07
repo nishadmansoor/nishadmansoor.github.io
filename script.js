d570eecfc614e9a95a6b5f5c20bcddf8118ff95f
Nishad Mansoor <nishadm9802@gmail.com>
2026-08-07 15:45:20 -0400

    new layout

--- script.js
+++ script.js
@@ -19,11 +19,3 @@ document.querySelectorAll('#nav-links a').forEach(link => {
     });
 });
 
-// fading header on scroll 
-const aboutSection = document.getElementById('about');
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach(entry => {
-        document.body.classList.toggle('past-about', !entry.isIntersecting);
-    });
-}, { threshold: 0 });
-aboutSection.observe(aboutSection);
