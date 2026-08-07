9fbfb6b5702bfe4e3e751a43495a3cb5bd907a5d
Nishad Mansoor <nishadm9802@gmail.com>
2026-08-07 01:12:07 -0400

    update layout

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//adding typewriter functionality to splash screen 
+// implementing typewriter functionality
 function typeText(element, text, speed, callback) {
     let i = 0;
     function type() {
@@ -12,24 +12,6 @@ function typeText(element, text, speed, callback) {
     }
     type();
 }
-// implementing typewriter effect on splash screen
-const line1 = document.getElementById('line1');
-const line2 = document.getElementById('line2');
-const line3 = document.getElementById('line3');
-const explore = document.getElementById('explore');
-
-//hero page
-typeText(line1, "hey there! my name is nishad", 60, function () {
-    setTimeout(function () {
-        typeText(line2, "welcome to my portfolio", 60, function () {
-            setTimeout(function () {
-                typeText(line3, "click here to learn more about me", 60, function () {
-                    explore.style.display = 'block';
-                }, 300);
-            }, 400);
-        });
-    }, 400);
-});
 
 document.querySelectorAll('#nav-links a').forEach(link => {
     link.addEventListener('click', function () {
