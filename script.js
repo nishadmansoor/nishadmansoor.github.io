6cb306b204a4aa13c5b6ff522ce6468ff1f97302
Nishad Mansoor <nishadm9802@gmail.com>
2026-08-24 00:24:37 -0400

    center content

--- script.js
+++ script.js
@@ -1,36 +1,4 @@
-// implementing typewriter functionality
-function typeText(element, text, speed, callback) {
-    let i = 0;
-    function type() {
-        if (i < text.length) {
-            element.textContent += text.charAt(i);
-            i++;
-            setTimeout(type, speed);
-        } else if (callback) {
-            callback();
-        }
-    }
-    type();
-}
-
-document.getElementById('nav-icon').addEventListener('click', function (e) {
-    e.preventDefault();
-    var checkbox = document.getElementById('nav-toggle');
-    checkbox.checked = !checkbox.checked;
-});
-
-document.querySelectorAll('#nav-links a').forEach(link => {
-    link.addEventListener('click', function () {
-        document.getElementById('nav-toggle').checked = false;
-    });
-});
-
 window.addEventListener('scroll', () => {
-    const checkbox = document.getElementById('nav-toggle');
-    if (checkbox.checked) {
-        checkbox.checked = false;
-    }
-
     const heroBottom = document.getElementById('hero').getBoundingClientRect().bottom;
     document.body.classList.toggle('past-hero', heroBottom <= 0);
 });
@@ -46,4 +14,3 @@ document.querySelectorAll('.flip-card').forEach(card => {
         document.getElementById('beyond').classList.toggle('has-flipped', !wasFlipped);
     });
 });
-
